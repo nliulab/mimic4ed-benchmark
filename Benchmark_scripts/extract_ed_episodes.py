7d75bf728ba9acd4e45758b397d6e9927bb9d378
XIE FENG <xief@u.duke.nus.edu>
2021-09-27 21:43:24 +0800

    update

--- Benchmark_scripts/extract_ed_episodes.py
+++
@@ -1,8 +0,0 @@
-import argparse
-import yaml
-
-parser = argparse.ArgumentParser(description='Extract per-subject data from MIMIC-III CSV files.')
-parser.add_argument('mimic_ed_path', type=str, help='Directory containing MIMIC-III CSV files.')
-parser.add_argument('output_path', type=str, help='Directory where per-subject data should be written.')
-parser.add_argument('--event_tables', '-e', type=str, nargs='+', help='Tables from which to read events.',
-                    default=['admission', 'patients', 'transfer'])
