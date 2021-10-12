4398e2d05afe687722c756a28f81dda18b8c9da6
zhoujuncc1 <jun@ihpc-ci-aai-012.a-ihpc.shared-svc.local>
2021-10-12 09:15:42 +0800

    update medcode

--- Benchmark_scripts/medcode_utils.py
+++ Benchmark_scripts/medcode_utils.py
@@ -2,35 +2,97 @@ import pandas as pd
 from datetime import timedelta
 from medcodes import charlson, elixhauser
 from medcodes.diagnoses._mappers import elixhauser_charlson
-map_dict = {'charlson':list(elixhauser_charlson.charlson_codes_v9.keys()),'elixhauser':list(elixhauser_charlson.elixhauser_codes_v9.keys())}
-map_index = {k:{c:i for i,c in enumerate(map_d)} for k,map_d in map_dict.items()}
-empty_map_vector = {k:[0 for _ in map_i] for k,map_i in map_index.items()}
-    
+
+cci_var_map = {
+    'myocardial infarction': 'cci_MI',
+    'congestive heart failure': 'cci_CHF',
+    'peripheral vascular disease': 'cci_PVD',
+    'cerebrovascular disease': 'cci_Stroke',
+    'dementia': 'cci_Dementia',
+    'chronic pulmonary disease': 'cci_Pulmonary',
+    'rheumatic disease': 'cci_Rheumatic',
+    'peptic ulcer disease': 'cci_PUD',
+    'mild liver disease': 'cci_Liver1',
+    'diabetes without chronic complication': 'cci_DM1',
+    'diabetes with chronic complication': 'cci_DM2',
+    'hemiplegia or paraplegia': 'cci_Paralysis',
+    'renal disease': 'cci_Renal',
+    'malignancy': 'cci_Cancer1',
+    'moderate or severe liver disease': 'cci_Liver2',
+    'metastatic solid tumor': 'cci_Cancer2',
+    'AIDS/HIV': 'cci_HIV'
+}
+
+eci_var_map = {
+'congestive heart failure' : 'eci_CHF',
+'cardiac arrhythmias' : 'eci_Arrhythmia',
+'valvular disease' : 'eci_Valvular',
+'pulmonary circulation disorders' : 'eci_PHTN',
+'peripheral vascular disorders' : 'eci_PVD',
+'hypertension, complicated' : 'eci_HTN1',
+'hypertension, uncomplicated' : 'eci_HTN2',
+'paralysis' : 'eci_Paralysis',
+'other neurological disorders' : 'eci_NeuroOther',
+'chronic pulmonary disease' : 'eci_Pulmonary',
+'diabetes, complicated' : 'eci_DM1',
+'diabetes, uncomplicated' : 'eci_DM2',
+'hypothyroidism' : 'eci_Hypothyroid',
+'renal failure' : 'eci_Renal',
+'liver disease' : 'eci_Liver',
+'peptic ulcer disease excluding bleeding' : 'eci_PUD',
+'AIDS/HIV' : 'eci_HIV',
+'lymphoma' : 'eci_Lymphoma',
+'metastatic cancer' : 'eci_Tumor2',
+'solid tumor without metastasis' : 'eci_Tumor1',
+'rheumatoid arthritis' : 'eci_Rheumatic',
+'coagulopathy' : 'eci_Coagulopathy',
+'obesity' : 'eci_Obesity',
+'weight loss' : 'eci_WeightLoss',
+'fluid and electrolyte disorders' : 'eci_FluidsLytes',
+'blood loss anemia' : 'eci_BloodLoss',
+'deficiency anemia' : 'eci_Anemia',
+'alcohol abuse' : 'eci_Alcohol',
+'drug abuse' : 'eci_Drugs',
+'psychoses' : 'eci_Psychoses',
+'depression' : 'eci_Depression'
+}
+
+map_dict = {'charlson': cci_var_map,
+            'elixhauser': eci_var_map}
+empty_map_vector = {k:{v:0 for key, v in map_i.items()} for k, map_i in map_dict.items()}
+
 
 def commorbidity_set(icd, version, mapping='charlson'):
     c_list = []
-    if mapping=='charlson':
+    if mapping == 'charlson':
         for i, c in enumerate(icd):
             c_list.extend(charlson(c, version[i]))
-    elif mapping=='elixhauser':
+    elif mapping == 'elixhauser':
         for i, c in enumerate(icd):
             c_list.extend(elixhauser(c, version[i]))
     else:
-        raise ValueError(f"{mapping} is not a recognized mapping. It must be \'charlson\' or \'elixhauser\'")
+        raise ValueError(
+            f"{mapping} is not a recognized mapping. It must be \'charlson\' or \'elixhauser\'")
     return set(c_list)
 
-def commorbidity_vector(icd, version, mapping='charlson'):
+
+def commorbidity_dict(icd, version, mapping='charlson'):
     map_set = commorbidity_set(icd, version, mapping)
     map_vector = empty_map_vector[mapping].copy()
     for c in map_set:
-        map_vector[map_index[mapping][c]]=1
+        map_vector[map_dict[mapping][c]] = 1
     return map_vector
 
+
 def diagnosis_with_time(df_diagnoses, df_admissions):
-    df_diagnoses_with_adm = pd.merge(df_diagnoses, df_admissions.loc[:,['hadm_id', 'subject_id','dischtime']], on = ['subject_id','hadm_id'], how='left')
-    df_diagnoses_with_adm.loc[:,'dischtime'] = pd.to_datetime(df_diagnoses_with_adm.loc[:,'dischtime'])
-    df_diagnoses_sorted = df_diagnoses_with_adm.sort_values(['subject_id', 'dischtime']).reset_index()
+    df_diagnoses_with_adm = pd.merge(df_diagnoses, df_admissions.loc[:, [
+                                     'hadm_id', 'subject_id', 'dischtime']], on=['subject_id', 'hadm_id'], how='left')
+    df_diagnoses_with_adm.loc[:, 'dischtime'] = pd.to_datetime(
+        df_diagnoses_with_adm.loc[:, 'dischtime'])
+    df_diagnoses_sorted = df_diagnoses_with_adm.sort_values(
+        ['subject_id', 'dischtime']).reset_index()
     return df_diagnoses_sorted
+
 
 def commorbidity(df_master, df_diagnoses, df_admissions, timerange):
     timerange = timedelta(days=timerange)
@@ -40,38 +102,38 @@ def commorbidity(df_master, df_diagnoses, df_admissions, timerange):
 
     j_start = 0
     j_end = 0
-    prev_subject=None
+    prev_subject = None
     diagnoses = []
     versions = []
+    stay_ids = []
     # Loop through ED visits
     for i, row in df_master.iterrows():
+        stay_ids.append(row['stay_id'])
         if i % 10000 == 0:
             print('Process: %d/%d' % (i, len(df_master)), end='\r')
         # If new subject, find the start and end index of same subject in sorted admission df
         if row['subject_id'] != prev_subject:
-            j_start=j_end
-            while j_start<len(df_diagnoses_sorted) and df_diagnoses_sorted['subject_id'][j_start] < row['subject_id']:
-                j_start+=1
+            j_start = j_end
+            while j_start < len(df_diagnoses_sorted) and df_diagnoses_sorted['subject_id'][j_start] < row['subject_id']:
+                j_start += 1
             j_end = j_start
-            
-            while j_end<len(df_diagnoses_sorted) and df_diagnoses_sorted['subject_id'][j_end] == row['subject_id']:
-                j_end+=1
-            prev_subject=row['subject_id']
+
+            while j_end < len(df_diagnoses_sorted) and df_diagnoses_sorted['subject_id'][j_end] == row['subject_id']:
+                j_end += 1
+            prev_subject = row['subject_id']
         # Count number of previous admissions within the time range
         icd_list = []
         version_list = []
         for j in range(j_start, j_end):
-            if row['intime']>df_diagnoses_sorted['dischtime'][j] and row['intime']-df_diagnoses_sorted['dischtime'][j]<=timerange:
+            if row['intime'] > df_diagnoses_sorted['dischtime'][j] and row['intime']-df_diagnoses_sorted['dischtime'][j] <= timerange:
                 icd_list.append(df_diagnoses_sorted.loc[j, 'icd_code'])
                 version_list.append(df_diagnoses_sorted.loc[j, 'icd_version'])
         diagnoses.append(icd_list)
         versions.append(version_list)
-
-
-
-    df_master['cci'] = None
-    df_master['eci'] = None
+    
+    cci_eci=[]
     for i, code in enumerate(diagnoses):
-        df_master.at[i, 'cci'] = commorbidity_vector(code, versions[i], mapping='charlson')
-        df_master.at[i, 'eci'] = commorbidity_vector(code, versions[i], mapping='elixhauser')
+        cci_eci.append({'stay_id':stay_ids[i], **commorbidity_dict(code, versions[i], mapping='charlson'), **commorbidity_dict(code, versions[i], mapping='elixhauser')})
+    df_cci_eci = pd.DataFrame.from_records(cci_eci)
+    df_master = pd.merge(df_master, df_cci_eci, on='stay_id', how='left')
     return df_master
--- Benchmark_scripts/utils.py
+++ Benchmark_scripts/utils.py
@@ -2,6 +2,7 @@ import gzip
 import shutil
 from os import listdir
 from os.path import isfile, join
+import IPython
 def unzip_files(dir):
     gzfiles = [join(dir, f) for f in listdir(dir) if '.gz' in f and isfile(join(dir, f))]
     for f in gzfiles:
@@ -9,3 +10,11 @@ def unzip_files(dir):
             with open(f[:-3], 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
+# display large dataframes in an html iframe
+def ldf_display(df, lines=500):
+    txt = ("<iframe " +
+           "srcdoc='" + df.head(lines).to_html() + "' " +
+           "width=1000 height=500>" +
+           "</iframe>")
+
+    return IPython.display.HTML(txt)
