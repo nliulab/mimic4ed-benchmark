8b3ebffcb10d631cc9b2ae74c06d70d2264359b4
siqili0325 <siqili@umich.edu>
2021-10-15 16:00:09 +0800

    added score values

--- Benchmark_scripts/helpers.py
+++ Benchmark_scripts/helpers.py
@@ -361,3 +361,337 @@ def display_outliers_count(df_master, vitals_valid_range):
                    '> outlier_high': len(df_master[df_master[column] > column_range['outlier_high']])
             }, ignore_index=True)
     return display_df
+
+def add_score_CCI(df):
+    conditions = [
+        (df['age'] < 50),
+        (df['age'] >= 50) & (df['age'] <= 59),
+        (df['age'] >= 60) & (df['age'] <= 69),
+        (df['age'] >= 70) & (df['age'] <= 79),
+        (df['age'] >= 80)
+    ]
+    values = [0, 1, 2, 3, 4]
+    df['score_CCI'] = np.select(conditions, values)    
+    df['score_CCI'] = df['score_CCI'] + df['cci_MI'] + df['cci_CHF'] + df['cci_PVD'] + df['cci_Stroke'] + df['cci_Dementia'] + df['cci_Pulmonary'] + df['cci_PUD'] + df['cci_Rheumatic'] +df['cci_Liver1']*1 + df['cci_Liver2']*3 + df['cci_DM1'] + df['cci_DM2']*2 +df['cci_Paralysis']*2 + df['cci_Renal']*2 + df['cci_Cancer1']*2 + df['cci_Cancer2']*6 + df['cci_HIV']*6
+    print("Varibale 'add_score_CCI' succeffully added")
+
+def add_triage_MAP(df):
+    df['triage_MAP'] = df['triage_sbp']*1/3 + df['triage_dbp']*2/3
+    print("Varibale 'add_triage_MAP' succeffully added")
+
+def add_score_REMS(df):
+    conditions1 = [
+        (df['age'] < 45),
+        (df['age'] >= 45) & (df['age'] <= 54),
+        (df['age'] >= 55) & (df['age'] <= 64),
+        (df['age'] >= 65) & (df['age'] <= 74),
+        (df['age'] > 74)
+    ]
+    values1 = [0, 2, 3, 5, 6]
+    conditions2 = [
+        (df['triage_MAP'] > 159),
+        (df['triage_MAP'] >= 130) & (df['triage_MAP'] <= 159),
+        (df['triage_MAP'] >= 110) & (df['triage_MAP'] <= 129),
+        (df['triage_MAP'] >= 70) & (df['triage_MAP'] <= 109),
+        (df['triage_MAP'] >= 50) & (df['triage_MAP'] <= 69),
+        (df['triage_MAP'] < 49)
+    ]
+    values2 = [4, 3, 2, 0, 2, 4]
+    conditions3 = [
+        (df['triage_heartrate'] >179),
+        (df['triage_heartrate'] >= 140) & (df['triage_heartrate'] <= 179),
+        (df['triage_heartrate'] >= 110) & (df['triage_heartrate'] <= 139),
+        (df['triage_heartrate'] >= 70) & (df['triage_heartrate'] <= 109),
+        (df['triage_heartrate'] >= 55) & (df['triage_heartrate'] <= 69),
+        (df['triage_heartrate'] >= 40) & (df['triage_heartrate'] <= 54),
+        (df['triage_heartrate'] < 40)
+    ]
+    values3 = [4, 3, 2, 0, 2, 3, 4]
+    conditions4 = [
+        (df['triage_resprate'] > 49),
+        (df['triage_resprate'] >= 35) & (df['triage_resprate'] <= 49),
+        (df['triage_resprate'] >= 25) & (df['triage_resprate'] <= 34),
+        (df['triage_resprate'] >= 12) & (df['triage_resprate'] <= 24),
+        (df['triage_resprate'] >= 10) & (df['triage_resprate'] <= 11),
+        (df['triage_resprate'] >= 6) & (df['triage_resprate'] <= 9),
+        (df['triage_resprate'] < 6)
+    ]
+    values4 = [4, 3, 1, 0, 1, 2, 4]
+    conditions5 = [
+        (df['triage_o2sat'] < 75),
+        (df['triage_o2sat'] >= 75) & (df['triage_o2sat'] <= 85),
+        (df['triage_o2sat'] >= 86) & (df['triage_o2sat'] <= 89),
+        (df['triage_o2sat'] > 89)
+    ]
+    values5 = [4, 3, 1, 0]
+    df['score_REMS'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) + np.select(conditions5, values5)
+    print("Varibale 'Score_REMS' succeffully added")
+    
+def add_score_CART(df):
+    conditions1 = [
+        (df['age'] < 55),
+        (df['age'] >= 55) & (df['age'] <= 69),
+        (df['age'] >= 70) 
+    ]
+    values1 = [0, 4, 9]
+    conditions2 = [
+        (df['triage_resprate'] < 21),
+        (df['triage_resprate'] >= 21) & (df['triage_resprate'] <= 23),
+        (df['triage_resprate'] >= 24) & (df['triage_resprate'] <= 25),
+        (df['triage_resprate'] >= 26) & (df['triage_resprate'] <= 29),
+        (df['triage_resprate'] >= 30) 
+    ]
+    values2 = [0, 8, 12, 15, 22]
+    conditions3 = [
+        (df['triage_heartrate'] < 110),
+        (df['triage_heartrate'] >= 110) & (df['triage_heartrate'] <= 139),
+        (df['triage_heartrate'] >= 140) 
+    ]
+    values3 = [0, 4, 13]
+    conditions4 = [
+        (df['triage_dbp'] > 49),
+        (df['triage_dbp'] >= 40) & (df['triage_dbp'] <= 49),
+        (df['triage_dbp'] >= 35) & (df['triage_dbp'] <= 39),
+        (df['triage_dbp'] < 35) 
+    ]
+    values4 = [0, 4, 6, 13]
+    df['score_CART'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4)
+    print("Varibale 'Score_CART' succeffully added")
+    
+def add_score_NEWS(df):
+    conditions1 = [
+        (df['triage_resprate'] <= 8),
+        (df['triage_resprate'] >= 9) & (df['triage_resprate'] <= 11),
+        (df['triage_resprate'] >= 12) & (df['triage_resprate'] <= 20),
+        (df['triage_resprate'] >= 21) & (df['triage_resprate'] <= 24),
+        (df['triage_resprate'] >= 25) 
+    ]
+    values1 = [3, 1, 0, 2, 3]
+    conditions2 = [
+        (df['triage_o2sat'] <= 91),
+        (df['triage_o2sat'] >= 92) & (df['triage_o2sat'] <= 93),
+        (df['triage_o2sat'] >= 94) & (df['triage_o2sat'] <= 95),
+        (df['triage_o2sat'] >= 96) 
+    ]
+    values2 = [3, 2, 1, 0]
+    conditions3 = [
+        (df['triage_temperature'] <= 35),
+        (df['triage_temperature'] > 35) & (df['triage_temperature'] <= 36),
+        (df['triage_temperature'] > 36) & (df['triage_temperature'] <= 38),
+        (df['triage_temperature'] > 38) & (df['triage_temperature'] <= 39),
+        (df['triage_temperature'] > 39) 
+    ]
+    values3 = [3, 1, 0, 1, 2]
+    conditions4 = [
+        (df['triage_sbp'] <= 90),
+        (df['triage_sbp'] >= 91) & (df['triage_sbp'] <= 100),
+        (df['triage_sbp'] >= 101) & (df['triage_sbp'] <= 110),
+        (df['triage_sbp'] >= 111) & (df['triage_sbp'] <= 219),
+        (df['triage_sbp'] > 219) 
+    ]
+    values4 = [3, 2, 1, 0, 3]
+    conditions5 = [
+        (df['triage_heartrate'] <= 40),
+        (df['triage_heartrate'] >= 41) & (df['triage_heartrate'] <= 50),
+        (df['triage_heartrate'] >= 51) & (df['triage_heartrate'] <= 90),
+        (df['triage_heartrate'] >= 91) & (df['triage_heartrate'] <= 110),
+        (df['triage_heartrate'] >= 111) & (df['triage_heartrate'] <= 130),
+        (df['triage_heartrate'] > 130) 
+    ]
+    values5 = [3, 1, 0, 1, 2, 3]    
+    df['score_NEWS'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) + np.select(conditions5, values5)
+    print("Varibale 'Score_NEWS' succeffully added")
+    
+def add_score_NEWS2(df):   
+    conditions1 = [
+        (df['triage_resprate'] <= 8),
+        (df['triage_resprate'] >= 9) & (df['triage_resprate'] <= 11),
+        (df['triage_resprate'] >= 12) & (df['triage_resprate'] <= 20),
+        (df['triage_resprate'] >= 21) & (df['triage_resprate'] <= 24),
+        (df['triage_resprate'] >= 25) 
+    ]
+    values1 = [3, 1, 0, 2, 3]
+    conditions2 = [
+        (df['triage_temperature'] <= 35),
+        (df['triage_temperature'] > 35) & (df['triage_temperature'] <= 36),
+        (df['triage_temperature'] > 36) & (df['triage_temperature'] <= 38),
+        (df['triage_temperature'] > 38) & (df['triage_temperature'] <= 39),
+        (df['triage_temperature'] > 39) 
+    ]
+    values2 = [3, 1, 0, 1, 2]
+    conditions3 = [
+        (df['triage_sbp'] <= 90),
+        (df['triage_sbp'] >= 91) & (df['triage_sbp'] <= 100),
+        (df['triage_sbp'] >= 101) & (df['triage_sbp'] <= 110),
+        (df['triage_sbp'] >= 111) & (df['triage_sbp'] <= 219),
+        (df['triage_sbp'] > 219) 
+    ]
+    values3 = [3, 2, 1, 0, 3]
+    conditions4 = [
+        (df['triage_heartrate'] <= 40),
+        (df['triage_heartrate'] >= 41) & (df['triage_heartrate'] <= 50),
+        (df['triage_heartrate'] >= 51) & (df['triage_heartrate'] <= 90),
+        (df['triage_heartrate'] >= 91) & (df['triage_heartrate'] <= 110),
+        (df['triage_heartrate'] >= 111) & (df['triage_heartrate'] <= 130),
+        (df['triage_heartrate'] > 130) 
+    ]
+    values4 = [3, 1, 0, 1, 2, 3]   
+    df['score_NEWS2'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4)
+    print("Varibale 'Score_NEWS2' succeffully added")
+    
+def add_score_MEWS(df):     
+    conditions1 = [
+        (df['triage_sbp'] <= 70),
+        (df['triage_sbp'] >= 71) & (df['triage_sbp'] <= 80),
+        (df['triage_sbp'] >= 81) & (df['triage_sbp'] <= 100),
+        (df['triage_sbp'] >= 101) & (df['triage_sbp'] <= 199),
+        (df['triage_sbp'] > 199) 
+    ]
+    values1 = [3, 2, 1, 0, 2]
+    conditions2 = [
+        (df['triage_heartrate'] <= 40),
+        (df['triage_heartrate'] >= 41) & (df['triage_heartrate'] <= 50),
+        (df['triage_heartrate'] >= 51) & (df['triage_heartrate'] <= 100),
+        (df['triage_heartrate'] >= 101) & (df['triage_heartrate'] <= 110),
+        (df['triage_heartrate'] >= 111) & (df['triage_heartrate'] <= 129),
+        (df['triage_heartrate'] >= 130) 
+    ]
+    values2 = [2, 1, 0, 1, 2, 3]
+    conditions3 = [
+        (df['triage_resprate'] < 9),
+        (df['triage_resprate'] >= 9) & (df['triage_resprate'] <= 14),
+        (df['triage_resprate'] >= 15) & (df['triage_resprate'] <= 20),
+        (df['triage_resprate'] >= 21) & (df['triage_resprate'] <= 29),
+        (df['triage_resprate'] >= 30) 
+    ]
+    values3 = [2, 0, 1, 2, 3]
+    conditions4 = [
+        (df['triage_temperature'] < 35),
+        (df['triage_temperature'] >= 35) & (df['triage_temperature'] < 38.5),
+        (df['triage_temperature'] >= 38.5) 
+    ]
+    values4 = [2, 0, 2]        
+    df['score_MEWS'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) 
+    print("Varibale 'Score_MEWS' succeffully added")
+    
+def add_score_SERP2d(df): 
+    conditions1 = [
+        (df['age'] < 30),
+        (df['age'] >= 30) & (df['age'] <= 49),
+        (df['age'] >= 50) & (df['age'] <= 79),
+        (df['age'] >= 80)
+    ]
+    values1 = [0, 9, 13, 17]
+    conditions2 = [
+        (df['triage_heartrate'] < 60),
+        (df['triage_heartrate'] >= 60) & (df['triage_heartrate'] <= 69),
+        (df['triage_heartrate'] >= 70) & (df['triage_heartrate'] <= 94),
+        (df['triage_heartrate'] >= 95) & (df['triage_heartrate'] <= 109),
+        (df['triage_heartrate'] >= 110) 
+    ]
+    values2 = [3, 0, 3, 6, 10]
+    conditions3 = [
+        (df['triage_resprate'] < 16),
+        (df['triage_resprate'] >= 16) & (df['triage_resprate'] <= 19),
+        (df['triage_resprate'] >= 20) 
+    ]
+    values3 = [11, 0, 7]
+    conditions4 = [
+        (df['triage_sbp'] < 100),
+        (df['triage_sbp'] >= 100) & (df['triage_sbp'] <= 114),
+        (df['triage_sbp'] >= 115) & (df['triage_sbp'] <= 149),
+        (df['triage_sbp'] >= 150) 
+    ]
+    values4 = [10, 4, 1, 0]
+    conditions5 = [
+        (df['triage_dbp'] < 50),
+        (df['triage_dbp'] >= 50) & (df['triage_dbp'] <= 94),
+        (df['triage_dbp'] >= 95) 
+    ]
+    values5 = [5, 0, 1]
+    conditions6 = [
+        (df['triage_o2sat'] < 90),
+        (df['triage_o2sat'] >= 90) & (df['triage_o2sat'] <= 94),
+        (df['triage_o2sat'] >= 95) 
+    ]
+    values6 = [7, 5, 0]
+    df['score_SERP2d'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) + np.select(conditions5, values5) + np.select(conditions6, values6)
+    print("Varibale 'Score_SERP2d' succeffully added")
+
+def add_score_SERP7d(df): 
+    conditions1 = [
+        (df['age'] < 30),
+        (df['age'] >= 30) & (df['age'] <= 49),
+        (df['age'] >= 50) & (df['age'] <= 79),
+        (df['age'] >= 80)
+    ]
+    values1 = [0, 10, 17, 21]
+    conditions2 = [
+        (df['triage_heartrate'] < 60),
+        (df['triage_heartrate'] >= 60) & (df['triage_heartrate'] <= 69),
+        (df['triage_heartrate'] >= 70) & (df['triage_heartrate'] <= 94),
+        (df['triage_heartrate'] >= 95) & (df['triage_heartrate'] <= 109),
+        (df['triage_heartrate'] >= 110) 
+    ]
+    values2 = [2, 0, 4, 8, 12]
+    conditions3 = [
+        (df['triage_resprate'] < 16),
+        (df['triage_resprate'] >= 16) & (df['triage_resprate'] <= 19),
+        (df['triage_resprate'] >= 20) 
+    ]
+    values3 = [10, 0, 6]
+    conditions4 = [
+        (df['triage_sbp'] < 100),
+        (df['triage_sbp'] >= 100) & (df['triage_sbp'] <= 114),
+        (df['triage_sbp'] >= 115) & (df['triage_sbp'] <= 149),
+        (df['triage_sbp'] >= 150) 
+    ]
+    values4 = [12, 6, 1, 0]
+    conditions5 = [
+        (df['triage_dbp'] < 50),
+        (df['triage_dbp'] >= 50) & (df['triage_dbp'] <= 94),
+        (df['triage_dbp'] >= 95) 
+    ]
+    values5 = [4, 0, 2]
+    df['score_SERP7d'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) + np.select(conditions5, values5)
+    print("Varibale 'Score_SERP7d' succeffully added")
+    
+def add_score_SERP30d(df): 
+    conditions1 = [
+        (df['age'] < 30),
+        (df['age'] >= 30) & (df['age'] <= 49),
+        (df['age'] >= 50) & (df['age'] <= 79),
+        (df['age'] >= 80)
+    ]
+    values1 = [0, 8, 14, 19]
+    conditions2 = [
+        (df['triage_heartrate'] < 60),
+        (df['triage_heartrate'] >= 60) & (df['triage_heartrate'] <= 69),
+        (df['triage_heartrate'] >= 70) & (df['triage_heartrate'] <= 94),
+        (df['triage_heartrate'] >= 95) & (df['triage_heartrate'] <= 109),
+        (df['triage_heartrate'] >= 110) 
+    ]
+    values2 = [1, 0, 2, 6, 9]
+    conditions3 = [
+        (df['triage_resprate'] < 16),
+        (df['triage_resprate'] >= 16) & (df['triage_resprate'] <= 19),
+        (df['triage_resprate'] >= 20) 
+    ]
+    values3 = [8, 0, 6]
+    conditions4 = [
+        (df['triage_sbp'] < 100),
+        (df['triage_sbp'] >= 100) & (df['triage_sbp'] <= 114),
+        (df['triage_sbp'] >= 115) & (df['triage_sbp'] <= 149),
+        (df['triage_sbp'] >= 150) 
+    ]
+    values4 = [8, 5, 2, 0]
+    conditions5 = [
+        (df['triage_dbp'] < 50),
+        (df['triage_dbp'] >= 50) & (df['triage_dbp'] <= 94),
+        (df['triage_dbp'] >= 95) 
+    ]
+    values5 = [3, 0, 2]
+    df['score_SERP30d'] = np.select(conditions1, values1) + np.select(conditions2, values2) + np.select(conditions3, values3) +                              np.select(conditions4, values4) + np.select(conditions5, values5) + df['cci_Cancer1']*6 + df['cci_Cancer2']*12
+    print("Varibale 'Score_SERP30d' succeffully added")
+    
+    
