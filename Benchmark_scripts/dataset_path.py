176e4c88f4a0194a544bb800664cf9b480fce4db
zhoujuncc1 <zhoujuncc1@gmail.com>
2022-01-12 16:52:15 +0800

    rerun experiments

---
+++ Benchmark_scripts/dataset_path.py
@@ -0,0 +1,11 @@
+## Defining "argparse" parameters
+
+#For ZJ
+mimic_iv_path = '/dataset/mimiciv/1.0/'
+output_path = '/home/jun/data/MIMIC/processed/'
+
+#mimic_iv_path = '/Users/jin/Desktop/Temporal_AutoScore/Data/mimic-iv-1.0'
+#mimic_iv_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark/OriginalData/data'
+
+#output_path = '/Users/jin/Desktop/Temporal_AutoScore/Output/mimic-iv-1.0'
+#output_path = '/Users/siqili/Desktop/National University of Singapore/Liu Nan - WP_MIMIC_Benchmark'
